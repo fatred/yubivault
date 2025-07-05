7dff0b5a0df3e2f44399f94108b8b2265915db20
John Howard <john.howard@proton.ch>
2025-07-05 18:22:26 +0200

    add yubicode

--- go.mod
+++ go.mod
@@ -1,19 +1,28 @@
 module github.com/fatred/yubivault
 
-go 1.23.0
+go 1.23.6
 
 toolchain go1.24.4
 
 require github.com/hashicorp/vault-client-go v0.4.3
 
 require (
+	github.com/miekg/pkcs11 v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/thales-e-security/pool v0.0.2 // indirect
+)
+
+require (
+	github.com/ThalesGroup/crypto11 v1.4.1
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
+	golang.org/x/crypto v0.39.0
 	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0
 	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"syscall"
 	"time"
 
+	"github.com/ThalesGroup/crypto11"
 	"github.com/hashicorp/vault-client-go"
 	"github.com/hashicorp/vault-client-go/schema"
+	"golang.org/x/term"
 	"gopkg.in/yaml.v2"
 )
 
 type AppConfig struct {
-	VaultAddr       string `yaml:"vaultAddr"`
-	CertAuthName    string `yaml:"certAuthName"`
-	CertAuthMount   string `yaml:"certAuthMount"`
-	CertAuthPemFile string `yaml:"certAuthPemFile"`
-	CertAuthKeyFile string `yaml:"certAuthKeyFile"`
+	VaultAddr        string `yaml:"vaultAddr"`
+	CertAuthName     string `yaml:"certAuthName"`
+	CertAuthMount    string `yaml:"certAuthMount"`
+	CertAuthPemFile  string `yaml:"certAuthPemFile"`
+	CertAuthKeyFile  string `yaml:"certAuthKeyFile"`
+	OpenScPath       string `yaml:"openscPath"`
+	YubikeySerial    string `yaml:"yubikeySerial"`
+	YubikeyPivSerial string `yaml:"yubikeyPivSerial"`
+	YubikeyPivLabel  string `yaml:"yubikeyPivLabel"`
+	YubikeyPivIndex  int    `yaml:"yubikeyPivIndex"`
 }
 
 func LoadConfig(homeDir string) (*AppConfig, error) {
@@ -38,34 +48,103 @@ func LoadConfig(homeDir string) (*AppConfig, error) {
 func main() {
 	ctx := context.Background()
 
-	var localFlag = flag.Bool("local", true, "Use cert and key found in ~/.vault/client-cert.pem|key")
-	var yubikeyFlag = flag.Bool("yubi", true, "User cert and key stored in a yubikey only")
+	var localFlag = flag.Bool("local", false, "Use cert and key found in ~/.vault/client-cert.pem|key")
+	var yubikeyFlag = flag.Bool("yubi", false, "User cert and key stored in a yubikey only")
+	flag.Parse()
 
 	homeDir, _ := os.UserHomeDir()
 
 	appConfig, err := LoadConfig(homeDir)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	tls := vault.TLSConfiguration{}
-	if *localFlag {
-		tls.ClientCertificate.FromFile = homeDir + "/.vault/" + appConfig.CertAuthPemFile
-		tls.ClientCertificateKey.FromFile = homeDir + "/.vault/" + appConfig.CertAuthKeyFile
-	}
-	if *yubikeyFlag {
-		tls.ClientCertificate.FromFile = homeDir + "/.vault/" + appConfig.CertAuthPemFile
-		tls.ClientCertificateKey.FromFile = homeDir + "/.vault/" + appConfig.CertAuthKeyFile
-	}
-
-	client, err := vault.New(
-		vault.WithAddress(appConfig.VaultAddr),
-		vault.WithTLS(tls),
-		vault.WithRequestTimeout(30*time.Second),
-	)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if (!*localFlag && !*yubikeyFlag) || (*localFlag && *yubikeyFlag) {
+		log.Fatal("Pick -local or -yubi next time")
+		os.Exit(-1)
+	}
+
+	var tlsConfig = vault.TLSConfiguration{}
+	var client *vault.Client
+
+	if *localFlag {
+		tlsConfig.ClientCertificate.FromFile = homeDir + "/.vault/" + appConfig.CertAuthPemFile
+		tlsConfig.ClientCertificateKey.FromFile = homeDir + "/.vault/" + appConfig.CertAuthKeyFile
+		client, err = vault.New(
+			vault.WithAddress(appConfig.VaultAddr),
+			vault.WithTLS(tlsConfig),
+			vault.WithRequestTimeout(30*time.Second),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if *yubikeyFlag {
+		tokenPin := os.Getenv("TOKEN_PIN")
+		if tokenPin == "" {
+			fmt.Print("PIN for " + appConfig.YubikeySerial + ": ")
+			bPin, err := term.ReadPassword(int(syscall.Stdin))
+			fmt.Println()
+			if err != nil {
+				log.Fatal("Could not read PIN code:", err)
+			}
+			tokenPin = string(bPin)
+			if tokenPin == "" {
+				log.Fatal("Need to enter PIN or set via $TOKEN_PIN")
+			}
+		}
+
+		cryptoCtx, err := crypto11.Configure(&crypto11.Config{
+			Path:        appConfig.OpenScPath,
+			TokenSerial: appConfig.YubikeyPivSerial,
+			Pin:         tokenPin,
+		})
+		if err != nil {
+			log.Fatal("Could not configure crypto11:", err)
+		}
+		defer cryptoCtx.Close()
+
+		kps, err := cryptoCtx.FindAllKeyPairs()
+		if err != nil || len(kps) == 0 {
+			log.Fatalf("Failed to find key pairs: %v", err)
+		}
+		if appConfig.YubikeyPivIndex >= len(kps) {
+			log.Fatalf("YubikeyPivIndex %d out of range", appConfig.YubikeyPivIndex)
+		}
+		signer := kps[appConfig.YubikeyPivIndex]
+
+		certs, err := cryptoCtx.FindAllPairedCertificates()
+		if err != nil {
+			log.Fatal("Could not search for certificates: ", err)
+		}
+		cert := certs[0]
+
+		tlsCert := tls.Certificate{
+			Certificate: cert.Certificate,
+			PrivateKey:  signer,
+			Leaf:        cert.Leaf,
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates:       []tls.Certificate{tlsCert},
+			InsecureSkipVerify: false,
+			MinVersion:         tls.VersionTLS12,
+		}
+
+		customClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: tlsConfig,
+			},
+			Timeout: 30 * time.Second,
+		}
+
+		client, err = vault.New(
+			vault.WithAddress(appConfig.VaultAddr),
+			vault.WithHTTPClient(customClient),
+		)
+		if err != nil {
+			log.Fatal("Failed to create Vault client:", err)
+		}
 	}
 
 	resp, err := client.Auth.CertLogin(ctx, schema.CertLoginRequest{Name: appConfig.CertAuthName}, vault.WithMountPath(appConfig.CertAuthMount))
